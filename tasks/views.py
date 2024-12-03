a07302ba17ee15dcfdf220ff2628bb412ee6e417
Brunowar12 <Brunowar12@users.noreply.github.com>
2024-12-03 12:28:42 +0200

    filter&ordering
    + tasks filter on title, due_date & is_favorite
    + tasks ordering on title due_date & is_favorite

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,14 @@
 import logging
 from django.shortcuts import render
+from django.utils.timezone import now
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics
 from rest_framework.decorators import action
+from rest_framework.exceptions import ValidationError
+from rest_framework.filters import OrderingFilter
 from rest_framework.response import Response
 from .serializers import TaskSerializer, CategorySerializer
+from .services import TaskService
 from .mixins import UserQuerysetMixin
 
 # Create a task
@@ -14,8 +18,19 @@ class TaskCreateView(UserQuerysetMixin, generics.CreateAPIView):
 # Task list
 class TaskListView(UserQuerysetMixin, generics.ListAPIView):
     serializer_class = TaskSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ["is_favorite"]
+    filter_backends = [DjangoFilterBackend, OrderingFilter]
+    filterset_fields = ["is_favorite", "title", "due_date"]
+    ordering_fields = ["is_favorite", "title", "due_date"]
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if TaskService.is_today_filter(self.request):
+            try:
+                return queryset.filter(due_date__date=now().date())
+            except Exception as e:
+                logging.error(f"Error filtering today's tasks: {e}")
+                raise ValidationError("Error filtering today's tasks")
+        return queryset
 
 # Task details, updates, and deletion
 class TaskDetailView(UserQuerysetMixin, generics.RetrieveUpdateDestroyAPIView):
@@ -25,8 +40,7 @@ class TaskDetailView(UserQuerysetMixin, generics.RetrieveUpdateDestroyAPIView):
     def toggle_favorite(self, request, pk=None):
         try:
             task = self.get_object()
-            task.is_favorite = not task.is_favorite
-            task.save()
+            TaskService.toggle_favorite(task)
             logging.info(f"Task {task.id} favorite status updated to {task.is_favorite}")
             return Response({"status": "favorite status updated", "is_favorite": task.is_favorite})
         except Exception as e:
