f50f0856e8b33e89daeea9d63b828d1310772aa4
Brunowar12 <128008317+Brunowar12@users.noreply.github.com>
2025-05-12 19:20:55 +0300

    fix
    + api_status with no permissions
    + requirements

--- api/views.py
+++ api/views.py
@@ -1,8 +1,10 @@
 from django.conf import settings
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
 @api_view(['GET'])
+@permission_classes([AllowAny])
 def api_status(request):
     return Response({
         "status": "ok",
